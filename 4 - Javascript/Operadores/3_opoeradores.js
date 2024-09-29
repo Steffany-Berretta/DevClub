047adbd8f5b34518fb0a622260460388c5b986f3
Steffany-Berretta <steffanyberretta@gmail.com>
2024-09-29 18:06:44 +0200

    1st commit

---
+++ 4 - Javascript/Operadores/3_opoeradores.js
@@ -0,0 +1,20 @@
+let result = 20;
+
+// result = result + 70;
+result  +=70
+
+console.log(result);
+result  -=70
+
+console.log(result);
+result  /=2
+
+console.log(result);
+result  %=3
+
+console.log(result);
+
+result  *=3
+
+console.log(result);
+
